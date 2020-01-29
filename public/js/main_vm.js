55007b82929a5d8b356d911feeae0c1b3721a045
Yuliia Kandych <yuliiakandych@YuliiaD13825685.fanshawec.ca>
2020-01-29 10:15:28 -0500

    basic functionality

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,15 +1,52 @@
 // imports always go first - if we're importing anything
+import ChatMessage from "./modules/ChatMessage.js";
 
 const socket = io();
-
-function setUserId(packet) {
+//this is data destructuring. Go look it up on MDN
+function setUserId({sID}) {
     //debugger;
-    console.log(packet);
+    console.log(sID);
+    vm.socketID = sID;
 }
 
 function showDisconnectMessage(){
     console.log('a user disconnected');
 }
 
+function appendMessage(message){
+    vm.messages.push(message);
+}
+
+const vm = new Vue({
+    data:{
+        socketID: "",
+        message:"",
+        nickname:"",
+        messages: []
+    },
+
+    methods:{
+
+        dispatchMessage(){
+            console.log('handle emit message');
+
+            socket.emit('chat_message', {
+                content: this.message,
+                name: this.nickname || "anonymous"
+            })
+            this.message="";
+        }
+    },
+
+    mounted: function(){
+        console.log('vue is done mounting');
+    },
+
+    components:{
+        newmessage: ChatMessage
+    }
+}).$mount("#app");
+
 socket.addEventListener('connected', setUserId);
-socket.addEventListener('disconnect', showDisconnectMessage)
+socket.addEventListener('disconnect', showDisconnectMessage);
+socket.addEventListener('new_message', appendMessage);
--- public/js/modules/ChatMessage.js
+++ public/js/modules/ChatMessage.js
@@ -2,5 +2,19 @@
 // will be made public when you import this file into another via the import statement
 
 export default {
+    props: ['msg'],
+
+    template:`
+        <li>
+            <p class="new-message">
+                <span>{{ msg.message.name }} says: </span>
+                {{ msg.message.content }}
+            </p>
+        </li>
+    `,
+
+    data:function(){
+        return { message: "hello from the template"};
+    }
 
 }
